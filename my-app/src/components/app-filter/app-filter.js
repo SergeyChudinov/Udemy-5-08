923780a2925c3b67e03238b20252d0becbb79b82
SergeyChudinov <sergeychudinov1986@gmail.com>
2022-07-27 11:16:42 +0500

    Make lesson-5-24

--- my-app/src/components/app-filter/app-filter.js
+++ my-app/src/components/app-filter/app-filter.js
@@ -2,20 +2,22 @@ import './app-filter.css'
 
 const AppFilter = (props) => {
     const buttonData = [
-        {name: 'all', label: 'Все сотрудники'},
-        {name: 'rise', label: 'На повышение'},
-        {name: 'moreThen1000', label: 'З/П больше 1000$'},
-        {name: 'lessThen1000', label: 'З/П меньше 1000$'}
+        {name: 'all', label: 'Все сотрудники', colored: false},
+        {name: 'rise', label: 'На повышение', colored: false},
+        {name: 'moreThen1000', label: 'З/П больше 1000$', colored: false},
+        {name: 'lessThen1000', label: 'З/П меньше 1000$', colored: true}
     ];
-    const buttons = buttonData.map(({name, label}) => {
+    const buttons = buttonData.map(({name, label, colored}) => {
         const active = props.filter === name;
         const clazz = active ? "btn btn-light" : "btn btn-outline-light";
+        const red = colored ? 'red' : '';
         return (
             <button 
             onClick={() => props.onFilterSelect(name)}
                 className={clazz}
                 type="button"
-                key={name}>
+                key={name}
+                style={{color: red}}>
                 {label}
             </button>
         )
